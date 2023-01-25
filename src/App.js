ca3b57f1854acb6de45d9ee4fa455e45d6441a27
GREENFONTS <godwillonyewuchii@gmail.com>
2023-01-25 14:25:34 +0100

    feat: React project setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
