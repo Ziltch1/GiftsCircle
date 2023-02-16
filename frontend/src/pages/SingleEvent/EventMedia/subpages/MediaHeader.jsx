b692ecd5875412f0e150a0d8e4a2207eb23c8706
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-02-16 03:39:40 +0100

    some changes

--- frontend/src/pages/SingleEvent/EventMedia/subpages/MediaHeader.jsx
+++ frontend/src/pages/SingleEvent/EventMedia/subpages/MediaHeader.jsx
@@ -9,11 +9,14 @@ const MediaHeader = ({ navPosition, setNavPosition }) => {
 
   return (
     <Box mb='5'>
-        <Heading mb='5' fontWeight={'medium'} fontSize={24}>Media</Heading>
+        <Flex justifyContent='space-between'>
+              <Heading mb='5' fontWeight={'medium'} fontSize={24}>Media</Heading>
+              <Button bg='#00BFB2' fontWeight={'medium'} color='white' fontSize='14px'>Upload images/videos</Button>
+        </Flex>
         <Box fontSize={14} fontWeight='semibold'>
             <Flex alignItems={'center'} gap={4}>
                   {actionBtns.map((btn, index) => <Button borderRadius={100} fontSize={14} bg='none' onClick={() => handleClick(index)} 
-                  style={index === navPosition ? { background: '#CCF2F0', padding: '8px 15px', borderRadius: '100px' } : {fontWeight: 'normal'}}>{btn}</Button>)}
+                      style={index === navPosition ? { background: '#CCF2F0', padding: '8px 15px', borderRadius: '100px', color: '#009F94' } : {fontWeight: 'bold'}}>{btn}</Button>)}
             </Flex>  
         </Box>
 
