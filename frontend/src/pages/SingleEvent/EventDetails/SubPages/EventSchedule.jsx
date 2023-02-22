5a18306d98118e1abf79ee9dad3d816848db653b
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-02-22 05:13:45 +0100

    conflict resolved

--- frontend/src/pages/SingleEvent/EventDetails/SubPages/EventSchedule.jsx
+++ frontend/src/pages/SingleEvent/EventDetails/SubPages/EventSchedule.jsx
@@ -72,41 +72,6 @@ const EventSchedule = ({ newEvent }) => {
             </Text>
           </Box>
 
-<<<<<<< HEAD
-                      Class #1: Introduction to copywriting
-                      Class #2: Brands, brand voice and the digital marketing ecosystem
-                      Class #3: Writing websites and emails
-                      Class #4: Writing for social and video scripts <br />
-
-                      By the end of our course, you’ll be a bona fide copywriter, fully equipped with the skill needed to launch like-able brands, write sales and marketing assets for every marketing channel, grow your business and quadruple your sales. <br />
-
-                      This course costs $1,000.00 per attendee.
-                </Text>
-
-                <Box w='610px' bg='#EEEEEE' h='270px' borderRadius={10} mb='7'></Box>
-
-                <Text fontSize={14} lineHeight='27px'>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et ut sodales semper consectetur sem. Commodo tristique eu ultrices eleifend commodo amet id. In orci, odio in eget viverra fermentum arcu. Lacus ultrices enim amet consequat dolor.
-                      Nibh nisl vel at morbi massa aliquet. Vitae porttitor felis feugiat turpis. Ut risus tortor, erat semper augue vel sit nisl, commodo. Sit sed urna mi, purus. Dui amet praesent et non morbi tincidunt tincidunt integer commodo. Eget turpis platea vitae tempor eget.
-                </Text>
-            </Box>
-
-            <Box w='295px' h='auto'>
-                <Button w='100%' mb='4' fontWeight={550} bg='#EEEEEE' fontSize={14} boxShadow='md' onClick={showDrawer}>Start a fund raising</Button>
-                <Box fontSize={14} mb='5' bg='#EEEEEE' borderRadius={5} p='5'>
-                    <Image src={clock} mb='2' />
-                    <Text>{newDate}</Text>
-                    <Text>{newEvent[0]?.start_time} – {newEvent[0]?.end_time} {newEvent[0]?.timezone}</Text>
-                </Box>
-
-                <Box fontSize={14} mb='5' bg='#EEEEEE' borderRadius={5} p='5'>
-                    <Image src={location} mb='2' />
-                    <Text lineHeight='25px' fontSize='14px'>{newDate} at {newEvent[0]?.venue}</Text>
-                </Box>
-                {openDrawer ? <Fundraising setOpenDrawer={setOpenDrawer} /> : null }
-            </Box>
-        </Flex>
-=======
           <Box fontSize={14} mb="5" bg="#EEEEEE" borderRadius={5} p="5">
             <Image src={location} mb="2" />
             <Text lineHeight="25px" fontSize="14px">
@@ -115,7 +80,6 @@ const EventSchedule = ({ newEvent }) => {
           </Box>
         </Box>
       </Flex>
->>>>>>> cd05b8d134d1ae955216fccbeb6cee2ad99edadd
     </Box>
   );
 };
