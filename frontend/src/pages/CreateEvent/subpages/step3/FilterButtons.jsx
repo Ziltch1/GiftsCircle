4b029774395f86f9652e0ea504196709c3c95eb4
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-02-27 21:18:16 +0100

    step 3 done

--- frontend/src/pages/CreateEvent/subpages/step3/FilterButtons.jsx
+++ frontend/src/pages/CreateEvent/subpages/step3/FilterButtons.jsx
@@ -1,4 +1,4 @@
-import { Button, Box, HStack } from '@chakra-ui/react'
+import { Button, Box, HStack, Flex } from '@chakra-ui/react'
 import React from 'react'
 import FilterSidebar from './subpages/FilterSidebar'
 
@@ -6,13 +6,17 @@ const FilterButtons = () => {
   const buttons = ['Wedding', 'Birthday', 'Gadgets', 'Kitchen', 'Graduation', 'Baby products', 'Retirement', 'Induction', 'Clothing']
   return (
     <Box mb='7'>
-      <HStack spacing={3}>
+      
+      <Flex justifyContent='space-between'>
         <FilterSidebar />
-        {buttons.map((btn) => 
-        <Button fontWeight='semibold' fontSize='12px' w='120px' bg='none' border='1.5px solid gray' _hover={{ bg: '#00BFB2', color: 'white', border: '1px solid #00BFB2', boxShadow: '0px 8px 30px rgba(0, 191, 178, 0.1)'}}>
-          {btn}
-        </Button>)}
-      </HStack>
+        <HStack spacing={3}>
+          {buttons.map((btn) => 
+          <Button fontWeight='semibold' fontSize='12px' w='100px' bg='none' border='1.5px solid gray' _hover={{ bg: '#00BFB2', color: 'white', border: '1px solid #00BFB2', boxShadow: '0px 8px 30px rgba(0, 191, 178, 0.1)'}}>
+            {btn}
+          </Button>)}
+        </HStack>
+      </Flex>
+      
     </Box>
   )
 }
--- frontend/src/pages/CreateEvent/subpages/step3/subpages/FilterSidebar.jsx
+++ frontend/src/pages/CreateEvent/subpages/step3/subpages/FilterSidebar.jsx
@@ -1,14 +1,21 @@
-import { Box, Select, FormControl } from '@chakra-ui/react'
-import React from 'react'
+import { AddIcon, ChevronDownIcon, CloseIcon,} from '@chakra-ui/icons'
+import { Box, Select, FormControl, Divider, Button, Text, Flex } from '@chakra-ui/react'
+import React, {useState} from 'react'
+import {FiSliders} from 'react-icons/fi'
+
 
 const FilterSidebar = () => {
+  const [toggleFilter, setToggleFilter] = useState(false)
+  const showFilter = () => {
+    setToggleFilter(!toggleFilter);
+  }
   return (
     <Box>
-      <FormControl>
-        <Select placeholder='Filter' bg='#555555' fontSize='14px' color='white' w='130px' textAlign='center'>
-          <option value=""></option>
-        </Select>
-      </FormControl>
+      <Button bg='#555555' fontSize='14px' color='white' w='130px' textAlign='center' onClick={showFilter}>
+        <Text mr='2'>Filter</Text>
+        <ChevronDownIcon fontSize={25} />
+      </Button>
+      {toggleFilter && <Filters />}
     </Box>
   )
 }
@@ -18,6 +25,82 @@ export default FilterSidebar
 export const Filters = () => {
   return (
     <>
+      <Box w='293px' h='900px' bg='white' mt='7' position='absolute' zIndex='2' borderRadius={10}>
+        <Box py='3' px='5'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Box display='flex' alignItems='center' justifyContent='space-between' gap={2}>
+            <FiSliders style={{fontWeight: 'bold', fontSize: '22px'}} />
+             <Text fontWeight='semibold'>Filter</Text>
+          </Box>
+          <CloseIcon fontSize={14} />
+          </Flex>
+        </Box>
+        <Divider />
+
+        <Box py='3' px='5' mt='2' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+
+        <Box py='3' px='5' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+
+        <Box py='3' px='5' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+
+        <Box py='3' px='5' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+
+        <Box py='3' px='5' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+
+        <Box py='3' px='5' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+
+        <Box py='3' px='5' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+
+        <Box py='3' px='5' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+
+        <Box py='3' px='5' mb='3'>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Text>Categories</Text>
+            <AddIcon />
+          </Flex>
+        </Box>
+      </Box>
     </>
   )
 }
+
