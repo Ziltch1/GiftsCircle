d6edfbe4b8c27b3fb1e615d17b3fbd49f7fd96f7
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-02-28 13:00:07 +0100

    step five done

--- frontend/src/pages/CreateEvent/subpages/step4/DeliveryDetailsForm.jsx
+++ frontend/src/pages/CreateEvent/subpages/step4/DeliveryDetailsForm.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import { Box, Flex, Text, Heading, Input, FormControl, FormLabel } from '@chakra-ui/react'
+import React, {useState} from 'react'
+import BackButton from '../BackButton'
 
 const DeliveryDetailsForm = () => {
+  const [address, setAddress] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [country, setCountry] = useState('')
+  const [postalCode, setPostalCode] = useState('')
+  const [phoneNumber1, setPhoneNumber1] = useState('')
+  const [phoneNumber2, setPhoneNumber2] = useState('')
   return (
-    <div>DeliveryDetailsForm</div>
+    <Box h="100%" overflow="auto" mb="16" mt='10' w="750px" mx="auto">
+      <Flex alignItems="start" justifyContent="space-between">
+        <Box>
+          <BackButton />
+        </Box>
+
+        <Box w="500px" mx="auto">
+          <Box mb='10'>
+            <Heading mb='2' fontWeight='semibold' fontSize='30px'>Delivery Details</Heading>
+            <Text color='#717171' fontSize={14}>Add the addresses you would like to receive your gifts</Text>
+          </Box>
+
+          <Box>
+            <FormControl>
+              <Box mb='6'>
+                <FormLabel fontWeight='semibold' fontSize={15}>Address</FormLabel>
+                <Input type='text' value={address} onChange={(e) => setAddress(e.target.value)}  />
+              </Box>
+
+              <Box mb='6'>
+                <FormLabel fontWeight='semibold' fontSize={15}>City</FormLabel>
+                <Input type='text' value={city} onChange={(e) => setCity(e.target.value)} />
+              </Box>
+
+              <Box mb='6'>
+                <FormLabel fontWeight='semibold' fontSize={15}>State</FormLabel>
+                <Input type='text' value={state} onChange={(e) => setState(e.target.value)} />
+              </Box>
+
+              <Box mb='6'>
+                <FormLabel fontWeight='semibold' fontSize={15}>Country</FormLabel>
+                <Input type='text' value={country} onChange={(e) => setCountry(e.target.value)} />
+              </Box>
+
+              <Box mb='6'>
+                <FormLabel fontWeight='semibold' fontSize={15}>Postal Code</FormLabel>
+                <Input type='text' value={postalCode} onChange={(e) => setPostalCode(e.target.value)} />
+              </Box>
+
+              <Box mb='6'>
+                <FormLabel fontWeight='semibold' fontSize={15}>Phone Number 1</FormLabel>
+                <Input type='text' value={phoneNumber1} onChange={(e) => setPhoneNumber1(e.target.value)} />
+              </Box>
+
+              <Box mb='6'>
+                <FormLabel fontWeight='semibold' fontSize={15}>Phone Number 2</FormLabel>
+                <Input type='text' value={phoneNumber2} onChange={(e) => setPhoneNumber2(e.target.value)} />
+              </Box>
+            </FormControl>
+          </Box>
+        </Box>
+      </Flex>
+    </Box>
   )
 }
 
--- frontend/src/pages/CreateEvent/subpages/step5/SummaryForm.jsx
+++ frontend/src/pages/CreateEvent/subpages/step5/SummaryForm.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import {
+  Box, Heading, Text, Flex, Checkbox, Input, FormLabel, Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  Image,
+  useDisclosure,
+  ModalFooter,
+  ModalHeader,
+  Avatar, AvatarBadge
+} from '@chakra-ui/react'
+import React, {useState} from 'react'
 
 const SummaryForm = () => {
+
+  const [openModal, setOpenModal] = useState(false)
+
+  const showModal = () => {
+    setOpenModal(true);
+  }
+
   return (
-    <div>SummaryForm</div>
+    <Box mt='8' w='80%' mx='auto' mb='16'>
+      {openModal && <ConfirmationModal setOpenModal={setOpenModal} />}
+      <Flex alignItems='center' justifyContent='space-between' mb='5'>
+        <Box>
+          <Heading mb='2' fontSize={30} fontWeight='semibold'>Summary</Heading>
+          <Text color='#717171' fontSize={14}>Review all you have been setting so far.</Text>
+        </Box>
+
+        <Box>
+          <Flex alignItems='center' gap={2} color='#00BFB2'>
+            <Text>Preview your event</Text>
+            <ExternalLinkIcon />
+          </Flex>
+        </Box>
+      </Flex>
+
+
+      <Box w='100%' bg='#EEEEEE' h='380px' borderRadius={5} mb='10'>
+
+      </Box>
+
+      <Box mb='10'>
+        <Heading mb='4' fontWeight={600} fontSize={18}>When should we publish your event?</Heading>
+        <Box mb='2' display='flex' alignItems='center' gap={2}>
+          <Checkbox colorScheme='teal' />
+          <Text fontSize={14}>Publish now</Text>
+        </Box>
+        <Box mb='2' display='flex' alignItems='center' gap={2}>
+          <Checkbox colorScheme='teal' />
+          <Text fontSize={14}>Schedule for later</Text>
+        </Box>
+      </Box>
+
+      <Box w='500px' mb='12'>
+        <Heading mb='4' fontWeight={600} fontSize={18}>Apply Donation to charity</Heading>
+        <FormLabel fontWeight={550} fontSize={14}>How many percentage do you want to add?</FormLabel>
+        <Input type='text' bg='#F4F4F4' placeholder='e.g 2%' fontSize={15} mb='3' />
+        <Box mb='2' display='flex' alignItems='center' gap={2}>
+          <Checkbox />
+          <Text fontSize={14}>Apply 2% to all cost of items to be donated to charity homes</Text>
+        </Box>
+      </Box>
+
+      <Box w='182px' mx='auto'>
+        <Button bg='#00BFB2' w='182px' fontSize={14} fontWeight={500} color='white' textAlign='center' onClick={showModal}>Finish</Button>
+      </Box>
+
+    </Box>
   )
 }
 
 export default SummaryForm
+
+
+export const ConfirmationModal = ({setOpenModal}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
+
+  const closeModal = () => {
+    setOpenModal(false)
+  }
+
+  return (
+    <>
+      <Modal closeOnOverlayClick={false} onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent maxW='500px' bg='white' pb='4'>
+          <Box>
+            {/* <ModalHeader fontSize={14}></ModalHeader> */}
+            <ModalCloseButton onClick={closeModal} />
+            <ModalBody>
+              <Heading>Enable Donations</Heading>
+              <Text>Are you sure you want to add 2% to all items cost to be donated to charity homes?</Text>
+              <Text fontSize={14} color='#0C4C84'>Copy link to share with other hosts</Text>
+              <Box mb='2' display='flex' alignItems='center' gap={2}>
+                <Checkbox />
+                <Text fontSize={14}>Apply 2% to all cost of items to be donated to charity homes</Text>
+              </Box>
+
+              <Button>Yes add 2% of cost items</Button>
+            </ModalBody>
+          </Box>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
