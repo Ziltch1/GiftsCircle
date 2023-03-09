bcdf75b371420e8c483d14d10723c164da5b585a
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-03-10 00:36:34 +0100

    some changes

--- frontend/src/pages/CreateEvent/subpages/step2/EventImageForm.jsx
+++ frontend/src/pages/CreateEvent/subpages/step2/EventImageForm.jsx
@@ -15,6 +15,7 @@ import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css';
 
 
+
 const EventImageForm = ({step, setStep}) => {
   const { newEvent } = useSelector(state => state.event);
   const [image, setImage] = useState('');
--- frontend/src/pages/SingleEvent/EventDetails/SubPages/EventSchedule.jsx
+++ frontend/src/pages/SingleEvent/EventDetails/SubPages/EventSchedule.jsx
@@ -25,42 +25,7 @@ const EventSchedule = ({ newEvent }) => {
             {newEvent.title}
           </Heading>
           <Text fontSize={14} lineHeight="27px" mb="4" dangerouslySetInnerHTML={{ __html: newEvent.descSummary }} />
-          {/* <Text fontSize={14} lineHeight="27px" mb="4">
-            Become a pro copywriter in 4 weeks. <br />
-            The NYC School of Copywriting is the only copy and content writing
-            masterclass designed to teach writers, marketers, small business
-            owners, startup founders and anyone else wanting to pick up one of
-            the most invaluable and sought out skills in the modern workplace.{' '}
-            <br />
-            This course focuses on the craft of conversational
-            copywriting—writing in a human voice that’s non-salesy, jargon and
-            buzzword free and ultimately styled to create and maintain authentic
-            connections with readers and customers. <br />
-            Organized and led by Corey Rosenberg, one of the most successful
-            marketing copywriters in Silicon Valley, the course is broken out
-            into 4 90 minute classes: <br />
-            Class #1: Introduction to copywriting Class #2: Brands, brand voice
-            and the digital marketing ecosystem Class #3: Writing websites and
-            emails Class #4: Writing for social and video scripts <br />
-            By the end of our course, you’ll be a bona fide copywriter, fully
-            equipped with the skill needed to launch like-able brands, write
-            sales and marketing assets for every marketing channel, grow your
-            business and quadruple your sales. <br />
-            This course costs $1,000.00 per attendee.
-          </Text> */}
-
           <Box w="610px" bg="#EEEEEE" h="270px" borderRadius={10} mb="7"></Box>
-
-          {/* <Text fontSize={14} lineHeight="27px">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et ut
-            sodales semper consectetur sem. Commodo tristique eu ultrices
-            eleifend commodo amet id. In orci, odio in eget viverra fermentum
-            arcu. Lacus ultrices enim amet consequat dolor. Nibh nisl vel at
-            morbi massa aliquet. Vitae porttitor felis feugiat turpis. Ut risus
-            tortor, erat semper augue vel sit nisl, commodo. Sit sed urna mi,
-            purus. Dui amet praesent et non morbi tincidunt tincidunt integer
-            commodo. Eget turpis platea vitae tempor eget.
-          </Text> */}
         </Box>
 
         <Box w="295px" h="auto">
