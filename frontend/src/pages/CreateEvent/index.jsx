606a2163458b2424a65447f9b464fa8443c74f77
GREENFONTS <godwillonyewuchii@gmail.com>
2023-03-30 15:45:25 +0100

    fix: fixed an error

--- frontend/src/pages/CreateEvent/index.jsx
+++ frontend/src/pages/CreateEvent/index.jsx
@@ -9,28 +9,27 @@ import AddGiftForm from './subpages/step3';
 import DeliveryDetailsForm from './subpages/step4/DeliveryDetailsForm';
 import SummaryForm from './subpages/step5/SummaryForm';
 import Stepper from './subpages/Stepper';
-import EventPreview from './subpages/EventPreview'
+import EventPreview from './subpages/EventPreview';
 import { useLocation } from 'react-router-dom';
 
 const Index = () => {
   const [step, setStep] = useState(1);
-  const location = useLocation();
 
   useEffect(() => {
     dispatch(GetGiftItems());
   }, []);
   return (
-    <Box py={step !==6 ? '4' : '0'}>
-      {step !== 6 && 
+    <Box py={step !== 6 ? '4' : '0'}>
+      {step !== 6 && (
         <>
-        <FormHeader step={step} setStep={setStep} />
-        <Stepper step={step} />
-        </> 
-      }
+          <FormHeader step={step} setStep={setStep} />
+          <Stepper step={step} />
+        </>
+      )}
       <Box>
         {step === 1 && <BasicForm step={step} setStep={setStep} />}
         {step === 2 && <EventImageForm step={step} setStep={setStep} />}
-        {step === 3 && <AddGiftForm step={step} setStep={setStep}/>}
+        {step === 3 && <AddGiftForm step={step} setStep={setStep} />}
         {step === 4 && <DeliveryDetailsForm step={step} setStep={setStep} />}
         {step === 5 && <SummaryForm setStep={setStep} />}
         {step === 6 && <EventPreview setStep={setStep} />}
