864192308b7e5ce5a551d7462184edb07ce654a0
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-04-07 02:44:18 +0100

    total amount of gifts added

--- frontend/src/pages/CreateEvent/subpages/step3/subpages/GiftDrawer.jsx
+++ frontend/src/pages/CreateEvent/subpages/step3/subpages/GiftDrawer.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Drawer,
   DrawerBody,
@@ -12,19 +12,43 @@ import {
   Heading, DrawerFooter, Switch, Flex
 } from '@chakra-ui/react';
 import GiftItem from './GiftItem';
+import { useSelector } from 'react-redux';
 
 const GiftDrawer = ({ setOpenDrawer, data, setData, setAddedGiftItems, setGiftItems }) => {
   const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
   const [enableContribution, setEnableContribution] = useState(false);
+  const {giftItems} = useSelector(state => state.gift)
+  const [filteredGifts, setFilteredGifts] = useState([]);
+  const [amount, setAmount] = useState(0);
+
+  useEffect(() => {
+    const filtered = giftItems.filter(k =>
+      data.some(j => j.giftItemId === k.id)
+    );
+    setFilteredGifts(filtered);
+
+    const sumOfAmount = filtered.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue.amount;
+    }, 0);
+
+    if (sumOfAmount !== amount) {
+      setAmount(sumOfAmount)
+    }
+  }, [giftItems, data, amount]);
+
+
   const btnRef = React.useRef();
   const closeModal = () => {
     setOpenDrawer(false);
   };
 
+
+
   const handleClick = () => {
     setEnableContribution(!enableContribution)
     console.log('clicked', enableContribution);
   }
+  
   console.log(enableContribution);
   return (
     <Box>
@@ -63,11 +87,11 @@ const GiftDrawer = ({ setOpenDrawer, data, setData, setAddedGiftItems, setGiftIt
           <Box textAlign='left' p='8'>
            <Flex direction='column'>
               <Box mb='5'>
-                <Text>Total price: </Text>
+                <Text fontWeight={600} fontSize={18}>Total price: ₦{amount}</Text>
               </Box>
               <Box>
                 <Flex alignItems='center' justifyContent='space-between'>
-                  <Text>Enable contribution</Text>
+                  <Text fontWeight={600} fontSize={18}>Enable contribution</Text>
                   <Box onClick={handleClick}>
                     <Switch colorScheme='teal' />
                   </Box>
