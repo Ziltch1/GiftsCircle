85d7c29a81b3f6634189ea591ff3d550d9ffbc94
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-04-20 05:58:44 +0100

    fundraising tab added

--- frontend/src/pages/SingleEvent/EventDetails/SubPages/Fundraising.jsx
+++ frontend/src/pages/SingleEvent/EventDetails/SubPages/Fundraising.jsx
@@ -11,14 +11,20 @@ import {
     useDisclosure,
     Image,
     Box,
-    Text, Heading, Textarea, Input, FormLabel, Checkbox
+    Text, Heading, Textarea, Input, FormLabel, Checkbox, FormControl
 } from '@chakra-ui/react'
+import axios from 'axios'
 
 const Fundraising = ({setOpenDrawer}) => {
-  const { isOpen, onOpen, onClose } = useDisclosure({defaultIsOpen: true})
+  const { isOpen, onOpen, onClose } = useDisclosure({defaultIsOpen: true});
+  const [amount, setAmount] = useState('')
   const btnRef = React.useRef()
   const closeModal = () => {
     setOpenDrawer(false)
+  }
+
+  const handleClick = () => {
+    axios.post('', {})
   }
   return (
       <>
@@ -38,39 +44,41 @@ const Fundraising = ({setOpenDrawer}) => {
                       <Heading fontWeight='medium' fontSize='25px'>Start a fundraising</Heading>
                   </DrawerHeader>
 
-                  <DrawerBody mt='7'>
-                      <Box fontSize={'12px'} mb='5'>
-                          <FormLabel fontSize={'14px'} fontWeight='semibold'>Fundraising title</FormLabel>
-                          <Input type='text' placeholder='e.g  funds for honeymoon' fontSize={'12px'} bg='#F4F4F4'  />
-                      </Box>
-                      <Box fontSize={'13px'} mb='5'>
-                          <FormLabel fontSize={'14px'} fontWeight='semibold'>Tell your story / Descripe the Event</FormLabel>
-                          <Textarea placeholder='Tell your readers why you need their help and what you plan to use your donations for.' size='lg' fontWeight='medium' h='100px' fontSize='11px' bg='#F4f4f4' />
-                      </Box>
-                      <Box fontSize={'13px'} mb='5'>
-                          <FormLabel fontSize={'14px'} fontWeight='semibold'>Upload images</FormLabel>
-                          <FormLabel htmlFor='upload' w='100%' h='70px' bg='#f4f4f4' borderRadius={5} display='flex' justifyContent='center' alignItems='center '>
-                              <Text fontSize={11} color='#A8A8A8'>Drag and drop files or Click to Browse</Text>
-                              <Input type='file' id='upload' display='none' />
-                          </FormLabel>
-                      </Box>
-                      
-                      <Box fontSize={'13px'} mb='5'>
-                          <FormLabel fontSize={'14px'} fontWeight='semibold'>Enter amount</FormLabel>
-                          <Input type='text' placeholder='e.g dayo.abdullahi@gmail.com' fontSize={'12px'} bg='#F4F4F4' />
-                      </Box>
+                  <FormControl isRequired>
+                      <DrawerBody mt='4'>
+                          <Box fontSize={'12px'} mb='5'>
+                              <FormLabel fontSize={'13px'} fontWeight='semibold'>Fundraising title</FormLabel>
+                              <Input type='text' color='#A8A8A8' placeholder='e.g  funds for honeymoon' fontSize={'12px'} bg='#F4F4F4' />
+                          </Box>
+                          <Box fontSize={'13px'} mb='5'>
+                              <FormLabel fontSize={'13px'} fontWeight='semibold'>Tell your story / Descripe the Event</FormLabel>
+                              <Textarea color='#A8A8A8' placeholder='Tell your readers why you need their help and what you plan to use your donations for.' size='lg' fontWeight='medium' h='100px' fontSize='11px' bg='#F4f4f4' />
+                          </Box>
+                          <Box fontSize={'13px'} mb='5'>
+                              <FormLabel fontSize={'13px'} fontWeight='semibold'>Upload images</FormLabel>
+                              <FormLabel htmlFor='upload' w='100%' h='70px' bg='#f4f4f4' borderRadius={5} display='flex' justifyContent='center' alignItems='center '>
+                                  <Text fontSize={11} color='#A8A8A8'>Drag and drop files or Click to Browse</Text>
+                                  <Input type='file' color='#A8A8A8' id='upload' display='none' />
+                              </FormLabel>
+                          </Box>
 
-                      <Box fontSize={'13px'} mb='5'>
-                          <Checkbox colorScheme='green'><Text fontSize={12}>I agree to Event circle’s Terms and Privacy Policy on raising funds.</Text></Checkbox>
-                      </Box>
-                  </DrawerBody>
+                          <Box fontSize={'13px'} mb='5'>
+                              <FormLabel fontSize={'13px'} fontWeight='semibold'>Enter amount</FormLabel>
+                              <Input type='text' color='#A8A8A8' placeholder='e.g ₦ 587,500' fontSize={'12px'} bg='#F4F4F4' value={amount} onChange={(e) => setAmount(e.target.value)} />
+                          </Box>
 
-                  <DrawerFooter>
-                      <Button variant='outline' mr={3} onClick={onClose} color='white' bg='#00BFB2' fontWeight='medium' fontSize='13px'>
-                          Send message
-                      </Button>
-                  </DrawerFooter>
+                          <Box fontSize={'13px'} mb='5'>
+                              <Checkbox colorScheme='green' defaultChecked><Text fontSize={12}>I agree to Event circle’s Terms and Privacy Policy on raising funds.</Text></Checkbox>
+                          </Box>
+                      </DrawerBody>
 
+                      <DrawerFooter>
+                          <Button variant='outline' onCLick={handleClick} mr={3} onClick={onClose} color='white' bg='#00BFB2' fontWeight='medium' fontSize='13px'>
+                              Start fundraising
+                          </Button>
+                      </DrawerFooter>
+
+                  </FormControl>
               </DrawerContent>
           </Drawer>
       </>
