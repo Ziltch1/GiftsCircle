0cdd299ad132c25e9845001ead274da3bc2f7deb
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-04-30 22:13:56 +0100

    some changes in the marketplace

--- frontend/src/redux/axios/apis/asoebi.js
+++ frontend/src/redux/axios/apis/asoebi.js
@@ -1,5 +1,10 @@
 import api from '../axios';
 
+
+const AddEventAsoebiApi = (data) => {
+    return api.post('/asoebi/create', data);
+}
+
 const GetAsoebiItemsApi = () => {
     return api.get('/asoebiItem/Get/All');
 }
