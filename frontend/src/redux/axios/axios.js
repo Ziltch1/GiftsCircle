25c78931cea083c9985c826352a43c24a38843be
GREENFONTS <godwillonyewuchii@gmail.com>
2023-05-03 11:23:41 +0100

    feat: changed server

--- frontend/src/redux/axios/axios.js
+++ frontend/src/redux/axios/axios.js
@@ -8,9 +8,9 @@ const headers = {
   'Content-Type': 'application/json',
 };
 
-axios.defaults.baseURL = 'https://giftcircle-ws.onrender.com/api';
+axios.defaults.baseURL = 'https://gift-circle-server.onrender.com/api';
 //'http://localhost:4000/api
-//https://giftcircle-ws.onrender.com/api
+//https://gift-circle-server.onrender.com/api
 
 const axiosInstance = axios.create({
   headers,
