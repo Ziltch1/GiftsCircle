e7dfeb51c7b1e83df9cfa1d4696ca0b84a728d7b
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-05-15 12:27:45 +0100

    some changes

--- frontend/src/pages/Guest/subpages/EventMedia/subpages/MediaHeader.jsx
+++ frontend/src/pages/Guest/subpages/EventMedia/subpages/MediaHeader.jsx
@@ -1,46 +1,86 @@
 import React, { useState } from 'react';
-import { Box, Heading, Flex, FormLabel, Input } from '@chakra-ui/react';
-import { useUpload } from '../Hooks';
+import { Box, Heading, Flex, Button, FormLabel, Input, Stack } from '@chakra-ui/react';
 import LoadingModal from '../../../components/LoadingModal';
+import { useUpload } from '../Hooks';
+import AudioModal from './AudioModal';
+import VideoModal from './VideoModal';
+
 
 const MediaHeader = ({ navPosition, setNavPosition }) => {
-  // const actionBtns = ['Uploaded by me', 'Sent to me']
-  // const handleClick = (index) => {
-  //   setNavPosition(index);
-  // }
   const [image, setImage] = useState(null);
   const [modalOpen, setShowModal] = useState(false);
+  const [showAudioModal, setShowAudioModal] = useState(false);
+  const [showVideoModal, setShowVideoModal] = useState(false);
   const Data = useUpload(image, setShowModal, setImage);
+
+  const actionBtns = ['Uploaded by me', 'Sent to me'];
+  const handleClick = index => {
+    setNavPosition(index);
+  };
 
   return (
     <Box mb="5">
       <LoadingModal setShowModal={setShowModal} open={modalOpen} />
+      {showAudioModal && <AudioModal setShowAudioModal={setShowAudioModal} />}
+      {showVideoModal && <VideoModal setShowVideoModal={setShowVideoModal} />}
       <Flex justifyContent="space-between">
         <Heading mb="5" fontWeight={'medium'} fontSize={24}>
           Media
         </Heading>
-        <FormLabel
-          htmlFor="upload"
-          w="210px"
-          color="white"
-          bg="#00BFB2"
-          fontSize={14}
-          borderRadius="5px"
-          px="28px"
-          py="11px"
-          textAlign="center"
-        >
-          Send File to Organizer
-          <Input
-            type="file"
-            id="upload"
-            display="none"
-            multiple={true}
-            onChange={e => setImage(e.target.files)}
-          />
-        </FormLabel>
+
+        <Stack direction="row" spacing={4}>
+          <Button
+            onClick={() => setShowAudioModal(true)}
+            w="150px"
+            h='45px'
+            color="white"
+            bg="#00BFB2"
+            fontSize={14}
+            borderRadius="5px"
+            px="28px"
+            py="11px"
+            textAlign="center"
+          >
+            Record audio
+          </Button>
+          <Button
+            onClick={() => setShowVideoModal(true)}
+            w="150px"
+            h='45px'
+            color="white"
+            bg="#00BFB2"
+            fontSize={14}
+            borderRadius="5px"
+            px="28px"
+            py="11px"
+            textAlign="center"
+          >
+            Record video
+          </Button>
+          <FormLabel
+            htmlFor="upload"
+            w="210px"
+            h='45px'
+            color="white"
+            bg="#00BFB2"
+            fontSize={14}
+            borderRadius="5px"
+            px="28px"
+            py="11px"
+            textAlign="center"
+          >
+            Upload images/videos
+            <Input
+              type="file"
+              id="upload"
+              display="none"
+              multiple={true}
+              onChange={e => setImage(e.target.files)}
+            />
+          </FormLabel>
+        </Stack>
       </Flex>
-      {/* <Box fontSize={14} fontWeight="semibold">
+      <Box fontSize={14} fontWeight="semibold">
         <Flex alignItems={'center'} gap={4}>
           {actionBtns.map((btn, index) => (
             <Button
@@ -51,11 +91,11 @@ const MediaHeader = ({ navPosition, setNavPosition }) => {
               style={
                 index === navPosition
                   ? {
-                      background: '#CCF2F0',
-                      padding: '8px 15px',
-                      borderRadius: '100px',
-                      color: '#009F94',
-                    }
+                    background: '#CCF2F0',
+                    padding: '8px 15px',
+                    borderRadius: '100px',
+                    color: '#009F94',
+                  }
                   : { fontWeight: 'bold' }
               }
             >
@@ -63,7 +103,7 @@ const MediaHeader = ({ navPosition, setNavPosition }) => {
             </Button>
           ))}
         </Flex>
-      </Box> */}
+      </Box>
     </Box>
   );
 };
