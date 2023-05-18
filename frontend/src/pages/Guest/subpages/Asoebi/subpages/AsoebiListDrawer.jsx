4e6cf136c991ae81ca1fc18c3eed68700a943a80
GREENFONTS <godwillonyewuchii@gmail.com>
2023-05-18 12:56:58 +0100

    feat: paystack popup

--- frontend/src/pages/Guest/subpages/Asoebi/subpages/AsoebiListDrawer.jsx
+++ frontend/src/pages/Guest/subpages/Asoebi/subpages/AsoebiListDrawer.jsx
@@ -1,34 +1,35 @@
-import React, {useState, useEffect, useC} from 'react'
+import React, { useState, useEffect } from 'react';
 import {
-    Drawer,
-    DrawerBody,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    DrawerFooter,
-    useDisclosure,
-    Box,
-    Text,
-    Heading, Flex, Image, Button
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerFooter,
+  useDisclosure,
+  Box,
+  Heading,
+  Flex,
+  Button,
 } from '@chakra-ui/react';
 import GiftListItem from './AsoebiListItem';
-import { useSelector } from 'react-redux';
 import { GetAsoebiItemsApi } from '../../../../../redux/axios/apis/asoebi';
+import PaymentButton from '../../../../../components/Buttons/PaymentButton';
 
-const AsoebiListDrawer = ({setShowListDrawer, asoebiCart, setAsoebiCart,}) => {
+const AsoebiListDrawer = ({ setShowListDrawer, asoebiCart, setAsoebiCart }) => {
   const [newAsoebi, setNewAsoebi] = useState([]);
   const [data, setData] = useState([]);
 
-  const getAsoebi = async() => {
+  const getAsoebi = async () => {
     try {
       const res = await GetAsoebiItemsApi();
       const data = await res.data;
-      setData(data)
+      setData(data);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getAsoebi();
@@ -36,14 +37,13 @@ const AsoebiListDrawer = ({setShowListDrawer, asoebiCart, setAsoebiCart,}) => {
 
   let asoebiAmount = 0;
 
+  useEffect(() => {
+    const asoebiCartSet = new Set(asoebiCart);
+    const filteredArray = data.filter(obj => asoebiCartSet.has(obj.id));
+    setNewAsoebi(filteredArray);
+  }, [asoebiCart, data]);
 
-    useEffect(() => {
-        const asoebiCartSet = new Set(asoebiCart);
-        const filteredArray = data.filter(obj => asoebiCartSet.has(obj.id));
-        setNewAsoebi(filteredArray);
-    }, [asoebiCart, data]);
-
- asoebiAmount = newAsoebi?.reduce((acc, curr) => acc + curr.amount, 0);
+  asoebiAmount = newAsoebi?.reduce((acc, curr) => acc + curr.amount, 0);
 
   const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
   const btnRef = React.useRef();
@@ -53,42 +53,49 @@ const AsoebiListDrawer = ({setShowListDrawer, asoebiCart, setAsoebiCart,}) => {
   };
 
   return (
-      <Box>
-          <Drawer
-              isOpen={isOpen}
-              placement="right"
-              onClose={onClose}
-              finalFocusRef={btnRef}
-              size="lg"
-              closeOnOverlayClick={false}
-          >
-              <DrawerOverlay />
-              <DrawerContent>
-                  <DrawerCloseButton onClick={closeModal} />
+    <Box>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="lg"
+        closeOnOverlayClick={false}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton onClick={closeModal} />
 
-                  <DrawerHeader>
-                      <Heading fontWeight="medium" fontSize="25px" mb="2">
-                          Summary of Purchase
-                      </Heading>
-                  </DrawerHeader>
+          <DrawerHeader>
+            <Heading fontWeight="medium" fontSize="25px" mb="2">
+              Summary of Purchase
+            </Heading>
+          </DrawerHeader>
 
-                  <DrawerBody>
-                      <Flex justifyContent='space-between' flexWrap='wrap' mb='5'>
-                          {newAsoebi.map(ele => <GiftListItem id={ele.id} item={ele} asoebiCart={asoebiCart} setAsoebiCart={setAsoebiCart} />)}
-                      </Flex>
-                      <Box mb='5' textAlign='right'>
-                          <Heading fontWeight="medium" fontSize="18px" mb="2">
-                              Subtotal (₦{asoebiAmount})
-                          </Heading>
-                      </Box>
-                  </DrawerBody>
-                  <DrawerFooter borderTop='1px solid lightgray'>
-                      <Button fontSize={13} color='white' ml='5' fontWeight='medium' bg='#00BFB2'>Checkout (₦{asoebiAmount})</Button>
-                  </DrawerFooter>
-              </DrawerContent>
-          </Drawer>
-      </Box>
-  )
-}
+          <DrawerBody>
+            <Flex justifyContent="space-between" flexWrap="wrap" mb="5">
+              {newAsoebi.map(ele => (
+                <GiftListItem
+                  id={ele.id}
+                  item={ele}
+                  asoebiCart={asoebiCart}
+                  setAsoebiCart={setAsoebiCart}
+                />
+              ))}
+            </Flex>
+            <Box mb="5" textAlign="right">
+              <Heading fontWeight="medium" fontSize="18px" mb="2">
+                Subtotal (₦{asoebiAmount})
+              </Heading>
+            </Box>
+          </DrawerBody>
+          <DrawerFooter borderTop="1px solid lightgray">
+            <PaymentButton amount={asoebiAmount} action={closeModal} />
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </Box>
+  );
+};
 
-export default AsoebiListDrawer
+export default AsoebiListDrawer;
