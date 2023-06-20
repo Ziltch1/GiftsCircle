5a3920ec828e307f3dbae5ca12672fd82efdd88f
GREENFONTS <godwillonyewuchii@gmail.com>
2023-06-20 13:06:12 +0100

    fix: gifts

--- frontend/src/pages/Gift/index.jsx
+++ frontend/src/pages/Gift/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, createContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Stack, Skeleton } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 import { dispatch } from '../../redux/store';
@@ -21,14 +21,14 @@ import PurchasedBy from './subpages/PurchasedBy/subpages/PurchasedBy';
 import GiftAndSourvenir from './subpages/GiftAndSourvenir';
 import { GetUserMarketItems } from '../../redux/features/marketplace/service';
 
-export const SearchContext = React.createContext()
-
 const Events = () => {
   const { user } = useSelector(state => state.user);
   const [navPosition, setNavPosition] = useState(0);
   const [loading, setLoading] = useState(true);
   const { events } = useSelector(state => state.event);
-  const [filtered, setFiltered] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [data, setData] = useState(null);
   const { giftItems, sourvenirItems } = useSelector(state => state.gift);
 
   useEffect(() => {
@@ -46,25 +46,36 @@ const Events = () => {
   useEffect(() => {
     if (events) {
       setLoading(false);
-      setFiltered(events)
+      setData(events);
     }
-  }, [events]);
+  }, [events, loading, navPosition]);
 
-  const updateEvents = (e) => {
-    const newData = events.filter((item) => item.title.toLowerCase().startsWith(e.target.value.toLowerCase()));
-    // const newFilter = events.filter((item) => item.category.toLowerCase().startsWith(filterKeyword.toLowerCase()));
-    setFiltered(newData);
-  };
-
-
+  useEffect(() => {
+    if (events) {
+      if (filter === '' && searchQuery === '') {
+        setData(events);
+      } else {
+        const newData = events.filter(
+          item =>
+            item.title.toLowerCase().includes(searchQuery.toLowerCase()) &&
+            item.category?.toLowerCase().includes(filter.toLowerCase())
+        );
+        setData(newData);
+      }
+    }
+  }, [searchQuery, events, filter]);
   return (
     <Box bg="#F5F5F5" h="100%" pb="8">
       <Box w="100%" bg="#f5f5f5" h="100%" pb="5">
         <Box w="90%" mx="auto">
           <GiftHeader />
           <GiftTabs navPosition={navPosition} setNavPosition={setNavPosition} />
-          <SearchContext.Provider value={[filtered, updateEvents,]}>
-          <Search />
+          <Search
+            setSearchQuery={setSearchQuery}
+            searchQuery={searchQuery}
+            filter={filter}
+            setFilter={setFilter}
+          />
           {loading ? (
             <Stack spacing="20px">
               <Skeleton height="50px" width="100%" />
@@ -78,8 +89,8 @@ const Events = () => {
                   <SkeletonLoader />
                 ) : (
                   <>
-                    {navPosition === 0 && <PurchasedFor />}
-                    {navPosition === 1 && <PurchasedBy events={events} />}
+                    {navPosition === 0 && <PurchasedFor events={data} />}
+                    {navPosition === 1 && <PurchasedBy events={data} />}
                     {navPosition === 2 && (
                       <GiftAndSourvenir
                         sourvenir={sourvenirItems}
@@ -91,7 +102,6 @@ const Events = () => {
               </Box>
             </>
           )}
-          </SearchContext.Provider>
         </Box>
       </Box>
     </Box>
