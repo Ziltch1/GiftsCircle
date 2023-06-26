2ef3cb6937cfb5ff9facb2e6779a268aa2da8362
GREENFONTS <godwillonyewuchii@gmail.com>
2023-06-26 09:19:31 +0100

    fix: prod issue

--- frontend/src/pages/SingleEvent/UserCheck.jsx
+++ frontend/src/pages/SingleEvent/UserCheck.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Guest from '../Guest';
 import Host from '.';
 import { Box } from '@chakra-ui/react';
@@ -12,9 +12,7 @@ const UserCheck = () => {
   const { id } = useParams();
   const { events } = useSelector(state => state.event);
   const { user } = useSelector(state => state.user);
-  const [activeUser, setActiveUser] = useState(false);
   const [newEvent, setNewEvent] = useState(null);
-  const navigate = useNavigate();
 
   let userId = user?.id;
 
@@ -27,23 +25,11 @@ const UserCheck = () => {
     }
   }, [events, id, userId]);
 
-  if(user?.id !== null){
-    setActiveUser(true)
-  }else{
-    setActiveUser(false)
-    navigate('/signin')
-  }
 
   return (
     <>
-      {activeUser &&
-        <>  
-          <Response />
-          <Box>
-            {newEvent?.user_id === userId ? <Host /> : <Guest />}
-          </Box>
-        </>
-      }
+      <Response />
+      <Box>{newEvent?.user_id === userId ? <Host /> : <Guest />}</Box>
     </>
   );
 };
