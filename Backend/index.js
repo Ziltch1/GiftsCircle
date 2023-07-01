a5bf1215535d293cad7141b6a2594f87e7a068cf
GREEENFONTS <godwillonyewuchii@gmail.com>
2023-07-01 22:45:30 +0100

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -32,15 +32,14 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "http://localhost:3001",
-//       "https://giftscircle.netlify.app",
-//     ],
-//   })
-// );
+app.use(
+ cors({
+   origin: [
+  "http://localhost:3000",
+   "http://localhost:3001",
+   "https://giftscircle.netlify.app",
+   ],
+  }));
 
 app.get("/", async (req, res) => {
   console.log("enetred");
