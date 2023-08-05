aab8f364452d32a6cac9f3b2e14d2ff99c435456
GREENFONTS <godwillonyewuchii@gmail.com>
2023-08-05 06:05:52 +0100

    fix

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://gift-circle-server.onrender.com'
+    ? 'https://gifts-circle.herokuapp.com'
     : 'http://localhost:4000';
 
 export const socket = io(URL, { reconnectionDelay: 5000 }).connect();
